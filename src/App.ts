a26597e0b50ec30e25873557ffcf8d19ade303e5
fedor <yatskevichfyodor@gmail.com>
2019-04-07 00:41:26 +0300

    App component was divided

---
+++ src/App.ts
@@ -0,0 +1,27 @@
+import Header from './components/Header/Header.vue'
+import {Vue, Component} from "vue-property-decorator";
+
+const bg_img_green = require('./assets/images/bg_green.jpg')
+const bg_img_blue = require('./assets/images/bg_blue.jpg')
+const bg_img_red = require('./assets/images/bg_red.jpg')
+const bg_img_orange = require('./assets/images/bg_orange.jpg')
+const bg_img_purple = require('./assets/images/bg_purple.jpg')
+const bg_img_yellow = require('./assets/images/bg_yellow.jpg')
+const bg_img_turquoise = require('./assets/images/bg_turquoise.jpg')
+
+@Component({
+    components: { Header }
+})
+export default class App extends Vue {
+    data() {
+        return {
+            style: {
+                backgroundImage: `url(${getRandomImageName([bg_img_green, bg_img_blue, bg_img_red, bg_img_orange, bg_img_purple, bg_img_yellow, bg_img_turquoise])})`
+            }
+        }
+    }
+}
+
+function getRandomImageName(imageNames: Array<string>) {
+    return imageNames[Math.floor(Math.random() * imageNames.length)]
+}
--- src/App.vue
+++ src/App.vue
@@ -4,42 +4,5 @@
   </div>
 </template>
 
-<script>
-  import Header from './components/Header/Header.vue'
-
-  import bg_img_green from './assets/images/bg_green.jpg'
-  import bg_img_blue from './assets/images/bg_blue.jpg'
-  import bg_img_red from './assets/images/bg_red.jpg'
-  import bg_img_orange from './assets/images/bg_orange.jpg'
-  import bg_img_purple from './assets/images/bg_purple.jpg'
-  import bg_img_yellow from './assets/images/bg_yellow.jpg'
-  import bg_img_turquoise from './assets/images/bg_turquoise.jpg'
-
-  export default {
-    name: 'App',
-    components: {
-      Header
-    },
-    data() {
-      return {
-        style: {
-          backgroundImage: `url(${getRandomImageName([bg_img_green, bg_img_blue, bg_img_red, bg_img_orange, bg_img_purple, bg_img_yellow, bg_img_turquoise])})`
-        }
-      }
-    },
-    methods: {}
-  }
-
-  function getRandomImageName(imageNames) {
-    return imageNames[Math.floor(Math.random() * imageNames.length)]
-  }
-</script>
-
-<style scoped>
-  #app {
-    min-height: 100%;
-    width: 100%;
-    height: auto;
-    position: fixed;
-  }
-</style>
+<script src="./App.ts"></script>
+<style src="./App.css"></style>
