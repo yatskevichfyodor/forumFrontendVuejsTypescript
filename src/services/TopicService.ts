37cbffedf18e0bc3c408b3f2807e90c7392280e2
fedor <yatskevichfyodor@gmail.com>
2019-04-08 00:48:48 +0300

    TopicService improved

--- src/services/TopicService.ts
+++ src/services/TopicService.ts
@@ -1,12 +1,29 @@
 import axios from 'axios';
 
-export class TopicService {
+const serverPath: string = 'http://localhost:8080';
+const topicServicePath: string = serverPath + '/topic';
 
+export class TopicService {
     constructor(){
     }
 
-    getTopics() {
-        return axios.get('http://localhost:8080/topic/')
+    getAll(topicId: number) {
+        return axios.get(`${topicServicePath}/${topicId}`)
     }
 
+    get(topicId: number) {
+        return axios.get(`${topicServicePath}/`)
+    }
+
+    create(topic: any) {
+        return axios.post(topicServicePath, topic)
+    }
+
+    update(topic: any) {
+        return axios.put(topicServicePath, topic)
+    }
+
+    delete(topicId: number) {
+        return axios.delete(`${topicServicePath}/${topicId}`)
+    }
 }
