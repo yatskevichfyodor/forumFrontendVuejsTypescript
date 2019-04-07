a97ba7ca8a69bc5b57673a5bf67d648b83ba0104
fedor <yatskevichfyodor@gmail.com>
2019-04-08 00:39:46 +0300

    vue-router dependency added

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
-
-const routes = {
-  '/': App
-}
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
