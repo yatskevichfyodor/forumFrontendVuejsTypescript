4ed4f18a5dbdc0c4ec4a7851e00cc895090b585a
fedor <yatskevichfyodor@gmail.com>
2019-04-08 20:43:54 +0300

    TopicEdit component added

---
+++ src/components/TopicEdit/TopicEdit.ts
@@ -0,0 +1,21 @@
+import {Component, Vue} from "vue-property-decorator";
+import {TopicService} from "@/services/TopicService";
+
+const topicService = new TopicService()
+
+@Component
+export default class TopicEdit extends Vue {
+    topic: any = {
+        title: '',
+        comments: [
+            { content: '' }
+        ]
+    };
+    createTopic() {
+        topicService.create(this.topic).then((response: any) => {
+            console.log(response);
+        })
+    }
+
+}
+
---
+++ src/components/TopicEdit/TopicEdit.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container form-group mb-4 mt-4" id="topics-panel">
+        <input v-model="topic.title" placeholder="Title" class="form-control mb-1"/>
+        <textarea v-model="topic.comments[0].content" placeholder="Content" class="form-control mb-1"/>
+        <button @click="createTopic()" class="btn btn-light">Create</button>
+    </div>
+</template>
+
+<script src="./TopicEdit.ts"></script>
+<style src="./TopicEdit.css"></style>
