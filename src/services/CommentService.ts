979921136ec7109e840cf3311c4e312608ad5f23
fedor <yatskevichfyodor@gmail.com>
2019-04-14 22:12:25 +0300

    CommentService added

---
+++ src/services/CommentService.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const config = require('@/config');
+const API_URI = config.API_URI;
+const SERVICE_URI: string = API_URI + '/comment';
+
+export class CommentService {
+    constructor(){
+    }
+
+    getAll() {
+        return axios.get(`${SERVICE_URI}/`)
+    }
+
+    get(id: number) {
+        return axios.get(`${SERVICE_URI}/${id}`)
+    }
+
+    create(dto: any) {
+        return axios.post(SERVICE_URI, dto)
+    }
+
+    update(dto: any) {
+        return axios.put(SERVICE_URI, dto)
+    }
+
+    delete(id: number) {
+        return axios.delete(`${SERVICE_URI}/${id}`)
+    }
+
+    getByTopicId(topicId: number) {
+        return axios.get(`${SERVICE_URI}/findByTopicId/${topicId}`)
+    }
+}
